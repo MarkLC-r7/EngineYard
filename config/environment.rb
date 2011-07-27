ec776a884554fc145c2e72d700fbd79b40e32343
Mark Lacomber <marklacomber@gmail.com>
2011-07-27 11:19:02 +0100

    First engine yard log test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EngineYard::Application.initialize!
+
+Rails.logger = Le.new('ac3dc54c-2084-4c00-9792-2de17e0043bc', 'EngineYard/EngineYard.log')
+
+
