a484162b4335cb25e43bb92920dc3a81d71c223a
Mark Lacomber <marklacomber@gmail.com>
2011-07-27 12:15:43 +0100

    timestamp in msg test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-	Rails.logger.warn("Home page on ACTUAL engineyard app was opened")
+	Rails.logger.warn("Wed Jul 27 04:16:30 -0700 2011")
   end
 
 end
