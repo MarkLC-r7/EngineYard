b1e5770774dec1e3d2259e24f76213b663028f3f
Mark Lacomber <marklacomber@gmail.com>
2011-07-27 10:38:53 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngineYard::Application.config.secret_token = 'ff0606466adae735508bf9047bc67d620d2f4b29640a58b47a37438c6173878b895fab2ffaca07be01cd29f4df3cf92eec20fc3c218c47730ccda1fb9b93a4ac'
