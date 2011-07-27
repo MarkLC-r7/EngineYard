fc535d7f8539e4a42cbd4f4cfbef7749a05c8f96
Mark Lacomber <marklacomber@gmail.com>
2011-07-27 12:33:14 +0100

    reset message2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-	Rails.logger.warn("Wed Jul 27 04:16:30 -0700 2011")
+	Rails.logger.warn("Logged from EngineYard HomePage Controller")
   end
 
 end
